cfc708ed7d17d3341663ba880fe743b0cd85c222
Kenean Dita <KeneanSufa@gmail.com>
2025-06-25 20:20:04 +0300

    JS script

---
+++ static/js/script.js
@@ -0,0 +1,14 @@
+function updateProducts() {
+    const category = document.getElementById("category").value;
+    const productDropdown = document.getElementById("product");
+    productDropdown.innerHTML = '<option value="">--Select Product--</option>';
+
+    if (productMap[category]) {
+        productMap[category].forEach(function(product) {
+            const option = document.createElement("option");
+            option.value = product;
+            option.textContent = product;
+            productDropdown.appendChild(option);
+        });
+    }
+}
