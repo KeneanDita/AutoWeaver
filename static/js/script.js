763f14ec1497cd346953bfd023a3b55b1a153a1d
Kenean Dita <KeneanSufa@gmail.com>
2025-06-26 19:45:48 +0300

    Changes made in script js

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,11 @@
 function updateProducts() {
     const category = document.getElementById("category").value;
     const productDropdown = document.getElementById("product");
-    productDropdown.innerHTML = '<option value="">--Select Product--</option>';
+
+    productDropdown.innerHTML = '<option value="">-- Select Product --</option>';
 
     if (productMap[category]) {
-        productMap[category].forEach(function(product) {
+        productMap[category].forEach(product => {
             const option = document.createElement("option");
             option.value = product;
             option.textContent = product;
